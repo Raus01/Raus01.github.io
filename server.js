fa9ea25f59459d2c1e92ce1a6c80ee77aa640909
Raus01 <rauslauri7@gmail.com>
2023-05-16 14:35:40 +0300

    koik

---
+++ server.js
@@ -0,0 +1,52 @@
+import express, { json } from 'express';
+//const fetch = (await import('node-fetch')).default;
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000; // Change this to the port you want to use
+
+app.use(cors());
+
+app.use(json())
+
+let jwt_token = ""
+// Define a route for the proxy
+
+app.get('/proxy', async (req, res) => {
+  // Make the request to the remote API
+  console.log('app.get /proxy route initiated')
+
+  //gets values from the client
+  const authHeader = req.headers.authorization;
+  const encodedCredentials = authHeader.split(' ')[1];
+  const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString();
+  const [username, password] = decodedCredentials.split(':');
+  console.log(username)
+  // Make the request to the remote API
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Basic " + encodedCredentials
+  };
+
+  const response = await fetch('https://01.kood.tech/api/auth/signin', {
+    method: 'POST',
+    headers
+   // Pass the query parameters as the request body
+  });
+//const data = await response.json();
+
+  // Get the response data and send it back to the client
+  const data = await response.json();
+  jwt_token = data;
+  res.json({ token: jwt_token });
+
+  //console.log(jwt_token)
+  //res.send((data));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
